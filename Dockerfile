52c8ce6f43a20dee096eec740b70075609297e4b
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2023-05-14 12:30:37 +0900

    Add example for supabase vector store

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,8 @@ FROM node:18-bullseye-slim
 
 WORKDIR /app
 
-RUN corepack enable && corepack prepare pnpm@latest --activate
+RUN apt-get update && apt-get install -y \
+    python3 \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/* \
+    && corepack enable && corepack prepare pnpm@latest --activate
---
+++ src/vectorStores/supabase.ts
@@ -0,0 +1,30 @@
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase"
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
+import { createClient } from "@supabase/supabase-js"
+
+const privateKey = process.env.SUPABASE_PRIVATE_KEY
+if (!privateKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`)
+
+const url = process.env.SUPABASE_URL
+if (!url) throw new Error(`Expected env var SUPABASE_URL`)
+
+export const run = async () => {
+  const client = createClient(url, privateKey)
+
+  const vectorStore = await SupabaseVectorStore.fromTexts(
+    ["Hello world", "Bye bye", "What's this?"],
+    [{ id: 2 }, { id: 1 }, { id: 3 }],
+    new OpenAIEmbeddings(),
+    {
+      client,
+      tableName: "documents",
+      queryName: "match_documents",
+    }
+  )
+
+  const resultOne = await vectorStore.similaritySearch("Hello world", 1)
+
+  console.log(resultOne)
+}
+
+const res = await run()
